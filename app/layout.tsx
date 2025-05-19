92f0abc5eeafa825f63de5c802f551d9ba4a2b25
espoupou <espoirativon@gmail.com>
2025-05-20 00:19:25 +0100

    meta set

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,14 @@
 import './styles/globals.css';
 
+export const metadata = {
+  title: 'Kokou Espoir ATIVON',
+    icons: {
+        icon: '/favicon.ico',
+        shortcut: '/favicon.ico',
+        apple: '/favicon.ico',
+    },
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
